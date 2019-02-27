9add964f2fc900e56fd8599d3934d749a456de05
KeeganMc <47431359+KeeganMc@users.noreply.github.com>
2019-02-26 19:13:15 -0600

    Update discoerdbot.py

--- discoerdbot.py
+++ discoerdbot.py
@@ -3,31 +3,34 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
 import time
-import os
+import random
+from discord import Game
 
-Client = discord.Client()
+
+Client = discord.client
 client = commands.Bot(command_prefix = '!')
+Clientdiscord = discord.Client()
+
 
 @client.event
 async def on_ready():
-    await client.change_presence(game=Game(name='videos'))
+    await client.change_presence(game=Game(name='      '))
     print('Ready, Freddy') 
 
 
 @client.event
 async def on_message(message):
-    if message.content == 'Hi':
-        await client.send_message(message.channel,'Hello.')
-    if message.content == '!1273':
-        em = discord.Embed(description='')
-        em.set_image(url='down the Rockefeller street ')
-        await client.send_message(message.channel, embed=em)
+    if message.content == '=help':
+        await client.send_message(message.channel,'``meme , help , prefix , info , bot``')
+    if message.content == '=':
+        await client.send_message(message.channel,'Invalid command')
+    if message.content == '=prefix':
+        await client.send_message(message.channel,'``=``')
+    if message.content == '=info':
+        await client.send_message(message.channel,'This bot was made by Rogue0730')
     if ('heck') in message.content:
        await client.delete_message(message)
-    if message.content.startswith('!memes'):
-        randomlist = ['https://cleanmemes.files.wordpress.com/2019/01/img_7510.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7511.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7512.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7513.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7514.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7474.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7478.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7241.jpg']
+    if message.content.startswith('=meme'):
+        randomlist = ['https://cleanmemes.files.wordpress.com/2019/01/img_7555.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7556.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7557.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7558.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7559.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7560.jpg','https://cleanmemes.files.wordpress.com/2019/01/img_7550.jpg']
         await client.send_message(message.channel,(random.choice(randomlist)))
-
-
 client.run(os.getenv('TOKEN'))
-                      
